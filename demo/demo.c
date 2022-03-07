3d153126325abe3c52524af2cf02bfd9a655be3c
Bernhard Bablok <bablokb@gmx.de>
2022-03-07 16:04:58 +0100

    changed name of PORT-macro

--- demo/demo.c
+++ demo/demo.c
@@ -21,7 +21,7 @@
 
 void init_hw() {
   stdio_init_all();
-  spi_init(SPI_TFT_PORT, 1000000);                // SPI with 1Mhz
+  spi_init(SPI_PORT, 1000000);                // SPI with 1Mhz
   gpio_set_function(SPI_RX, GPIO_FUNC_SPI);
   gpio_set_function(SPI_SCK,GPIO_FUNC_SPI);
   gpio_set_function(SPI_TX, GPIO_FUNC_SPI);
@@ -71,7 +71,7 @@ int main() {
   Test3();
 #endif
 #if defined(ENABLE_TEST4)
-  Test4(); 
+  Test4();
 #endif
 #if defined(ENABLE_TEST5)
   Test5();
